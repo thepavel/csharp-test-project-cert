c42dceaaa3d91d434bccceb4f93f04d5d95540fe
pavel <lutskovp@gmail.com>
2023-10-21 23:48:38 -0700

    floating point types

--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
 ﻿
-Console.WriteLine("");
+int[] data = new int[3];
 
-Console.WriteLine("Floating point types:");
-Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
-Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
-Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+string shortenedString = "Hello World!";
+Console.WriteLine(shortenedString);
 
