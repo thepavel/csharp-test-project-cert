1607dd538e7943968f7cb23f5cde64eed0231d97
pavel <lutskovp@gmail.com>
2023-10-22 21:50:57 -0700

    numeric precision and casting

--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
-﻿decimal myDecimal = 3.14m;
-Console.WriteLine($"decimal: {myDecimal}");
+﻿// decimal myDecimal = 3.14m;
+// Console.WriteLine($"decimal: {myDecimal}");
 
-int myInt = (int)myDecimal;
-Console.WriteLine($"int: {myInt}");
+// int myInt = (int)myDecimal;
+// Console.WriteLine($"int: {myInt}");
+
+
+decimal myDecimal = 1.23456789m;
+float myFloat = (float)myDecimal;
+
+Console.WriteLine($"Decimal: {myDecimal}");
+Console.WriteLine($"Float  : {myFloat}");
+
+/**** performing data conversions *****/
 
