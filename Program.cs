a4d7d699d09a6da4a6cad6225008fca6ba21481e
pavel <lutskovp@gmail.com>
2023-10-22 21:53:03 -0700

    tostring for ints

--- Program.cs
+++ Program.cs
@@ -5,11 +5,15 @@
 // Console.WriteLine($"int: {myInt}");
 
 
-decimal myDecimal = 1.23456789m;
-float myFloat = (float)myDecimal;
+// decimal myDecimal = 1.23456789m;
+// float myFloat = (float)myDecimal;
 
-Console.WriteLine($"Decimal: {myDecimal}");
-Console.WriteLine($"Float  : {myFloat}");
+// Console.WriteLine($"Decimal: {myDecimal}");
+// Console.WriteLine($"Float  : {myFloat}");
 
 /**** performing data conversions *****/
 
+int first = 5;
+int second = 7;
+string message = first.ToString() + second.ToString();
+Console.WriteLine(message);
