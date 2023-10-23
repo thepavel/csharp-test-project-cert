88b05a34a06feef6935b8871882664ed7a70c5ef
pavel <lutskovp@gmail.com>
2023-10-22 21:25:41 -0700

    widening conversion int to decimal

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
-﻿int first = 2;
-string second = "4";
-string result = first + second;
-Console.WriteLine(result);
+﻿// int first = 2;
+// string second = "4";
+// string result = first + second;
+// Console.WriteLine(result);
 
- 
+int myInt = 3;
+Console.WriteLine($"int: {myInt}");
+
+decimal myDecimal = myInt;
+Console.WriteLine($"decimal: {myDecimal}");
+
