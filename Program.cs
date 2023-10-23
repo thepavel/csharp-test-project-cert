090d67ff068b312388acf68352bd5ae1ec39e7f6
pavel <lutskovp@gmail.com>
2023-10-22 21:26:23 -0700

    convert decimal to int, losing precision

--- Program.cs
+++ Program.cs
@@ -1,11 +1,6 @@
-﻿// int first = 2;
-// string second = "4";
-// string result = first + second;
-// Console.WriteLine(result);
-
-int myInt = 3;
-Console.WriteLine($"int: {myInt}");
-
-decimal myDecimal = myInt;
+﻿decimal myDecimal = 3.14m;
 Console.WriteLine($"decimal: {myDecimal}");
+
+int myInt = (int)myDecimal;
+Console.WriteLine($"int: {myInt}");
 
