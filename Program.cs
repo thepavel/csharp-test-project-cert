a16d4b81b6c724a5622d2f96304f46ea6f38891b
pavel <lutskovp@gmail.com>
2023-10-22 22:14:42 -0700

    casting vs converting to int

--- Program.cs
+++ Program.cs
@@ -1,29 +1,49 @@
-﻿// decimal myDecimal = 3.14m;
-// Console.WriteLine($"decimal: {myDecimal}");
+﻿
 
-// int myInt = (int)myDecimal;
-// Console.WriteLine($"int: {myInt}");
+#region OldCode
+    // decimal myDecimal = 3.14m;
+    // Console.WriteLine($"decimal: {myDecimal}");
+    
+    // int myInt = (int)myDecimal;
+    // Console.WriteLine($"int: {myInt}");
+    
+    
+    // decimal myDecimal = 1.23456789m;
+    // float myFloat = (float)myDecimal;
+    
+    // Console.WriteLine($"Decimal: {myDecimal}");
+    // Console.WriteLine($"Float  : {myFloat}");
+    
+    /**** performing data conversions *****/
+    
+    /* int -> string
+    // int first = 5;
+    // int second = 7;
+    // string message = first.ToString() + second.ToString();
+    // Console.WriteLine(message);
+     */
+    
+    /* string -> int */
+    // string first = "5";
+    // string second = "7";
+    // int sum = int.Parse(first) + int.Parse(second);
+    // Console.WriteLine(sum);
+    
+#endregion
 
+/* Convert class usage, string -> int */
 
-// decimal myDecimal = 1.23456789m;
-// float myFloat = (float)myDecimal;
+//      string value1 = "5";
+//      string value2 = "7";
+//      int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
+//      Console.WriteLine(result);
+// Use Convert for downcasting to int because it rounds as expected rather than truncating 
 
-// Console.WriteLine($"Decimal: {myDecimal}");
-// Console.WriteLine($"Float  : {myFloat}");
+/* Compare casting and converting a decimal into an int */
 
-/**** performing data conversions *****/
+int value = (int)1.5m; // casting truncates
+Console.WriteLine(value);
 
-/* int -> string
-// int first = 5;
-// int second = 7;
-// string message = first.ToString() + second.ToString();
-// Console.WriteLine(message);
- */
-
-/* string -> int */
-string first = "5";
-string second = "7";
-int sum = int.Parse(first) + int.Parse(second);
-Console.WriteLine(sum);
-
+int value2 = Convert.ToInt32(1.5m); // converting rounds up correctly
+Console.WriteLine(value2);
 
