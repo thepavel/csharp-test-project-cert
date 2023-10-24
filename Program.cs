67ecdc8228a044a7edfdc9861b17349fe58c9978
pavel <lutskovp@gmail.com>
2023-10-23 22:33:34 -0700

    int.TryParse and out var scope

--- Program.cs
+++ Program.cs
@@ -48,7 +48,25 @@
 
 /* Int.Parse */
 
-string name = "Bob";
-Console.WriteLine(int.Parse(name));
+// string name = "Bob";
+// Console.WriteLine(int.Parse(name));
 
-// ^^ throws exception
+// // ^^ throws exception
+
+// 
+/* int.TryParse */
+//
+
+string value = "102";
+int result = 0;
+if (int.TryParse(value, out result))
+{
+    Console.WriteLine($"Measurement: {result}");
+}
+else
+{
+    Console.WriteLine("Unable to report the measurement.");
+}
+Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+
+
