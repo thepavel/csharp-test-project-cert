41ad761fe8268aa844c7b853deea36a574a4146a
pavel <lutskovp@gmail.com>
2023-10-23 22:52:53 -0700

    whitespace and curlies

--- Program.cs
+++ Program.cs
@@ -1,34 +1,34 @@
 ﻿
 
 #region OldCode
-    // decimal myDecimal = 3.14m;
-    // Console.WriteLine($"decimal: {myDecimal}");
-    
-    // int myInt = (int)myDecimal;
-    // Console.WriteLine($"int: {myInt}");
-    
-    
-    // decimal myDecimal = 1.23456789m;
-    // float myFloat = (float)myDecimal;
-    
-    // Console.WriteLine($"Decimal: {myDecimal}");
-    // Console.WriteLine($"Float  : {myFloat}");
-    
-    /**** performing data conversions *****/
-    
-    /* int -> string
-    // int first = 5;
-    // int second = 7;
-    // string message = first.ToString() + second.ToString();
-    // Console.WriteLine(message);
-     */
-    
-    /* string -> int */
-    // string first = "5";
-    // string second = "7";
-    // int sum = int.Parse(first) + int.Parse(second);
-    // Console.WriteLine(sum);
-    
+// decimal myDecimal = 3.14m;
+// Console.WriteLine($"decimal: {myDecimal}");
+
+// int myInt = (int)myDecimal;
+// Console.WriteLine($"int: {myInt}");
+
+
+// decimal myDecimal = 1.23456789m;
+// float myFloat = (float)myDecimal;
+
+// Console.WriteLine($"Decimal: {myDecimal}");
+// Console.WriteLine($"Float  : {myFloat}");
+
+/**** performing data conversions *****/
+
+/* int -> string
+// int first = 5;
+// int second = 7;
+// string message = first.ToString() + second.ToString();
+// Console.WriteLine(message);
+ */
+
+/* string -> int */
+// string first = "5";
+// string second = "7";
+// int sum = int.Parse(first) + int.Parse(second);
+// Console.WriteLine(sum);
+
 #endregion
 
 /* Convert class usage, string -> int */
@@ -68,7 +68,7 @@ else
     Console.WriteLine("Unable to report the measurement.");
 }
 
-if(result > 0)
-Console.WriteLine($"Measurement (w/ offset): {50 + result}");
-
-
+if (result > 0)
+{
+    Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+}
