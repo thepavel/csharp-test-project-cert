9695854fbdf4fc6224ac93bfeb83cf704a30cf2d
pavel <lutskovp@gmail.com>
2023-10-24 21:15:26 -0700

    adds basic array sorting with alphanumeric keys

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("console app blank");
+﻿string[] pallets = { "B14", "A11", "B12", "A13" };
+
+Console.WriteLine("Sorted...");
+Array.Sort(pallets);
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
