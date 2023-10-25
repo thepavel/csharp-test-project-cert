e77150605bf8f357a78a538d615891c0532f8835
pavel <lutskovp@gmail.com>
2023-10-24 22:54:53 -0700

    Array.Resize cuts from the end

--- Program.cs
+++ Program.cs
@@ -39,4 +39,12 @@ foreach (var pallet in pallets)
     Console.WriteLine($"-- {pallet}");
 }
 
+Console.WriteLine("");
+Array.Resize(ref pallets, 3);
+Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
+
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
 /* END */
