7520e60f2dbceeccba19f30b1d26097899da968f
pavel <lutskovp@gmail.com>
2023-10-24 21:23:20 -0700

    Array.Clear

--- Program.cs
+++ Program.cs
@@ -1,17 +1,33 @@
-﻿string[] pallets = { "B14", "A11", "B12", "A13" };
+﻿// string[] pallets = { "B14", "A11", "B12", "A13" };
 
-Console.WriteLine("Sorted...");
-Array.Sort(pallets);
-foreach (var pallet in pallets)
-{
-    Console.WriteLine($"-- {pallet}");
-}
+// Console.WriteLine("Sorted...");
+// Array.Sort(pallets);
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
 
+// Console.WriteLine("");
+// Console.WriteLine("Reversed...");
+// Array.Reverse(pallets);
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+/* Array.Clear */
+
+string[] pallets = { "B14", "A11", "B12", "A13" };
 Console.WriteLine("");
-Console.WriteLine("Reversed...");
-Array.Reverse(pallets);
+
+Console.WriteLine($"Before: {pallets[0]}");
+Array.Clear(pallets, 0, 2);
+Console.WriteLine($"After: {pallets[0]}");
+
+Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
 foreach (var pallet in pallets)
 {
     Console.WriteLine($"-- {pallet}");
 }
 
+/* END */
