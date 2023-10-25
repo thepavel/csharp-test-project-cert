533dfd550051251051eeada1c2439f35f618ea5d
pavel <lutskovp@gmail.com>
2023-10-24 22:50:55 -0700

    Array.Clear

--- Program.cs
+++ Program.cs
@@ -20,14 +20,14 @@
 string[] pallets = { "B14", "A11", "B12", "A13" };
 Console.WriteLine("");
 
-Console.WriteLine($"Before: {pallets[0]}");
-Array.Clear(pallets, 0, 2);
-Console.WriteLine($"After: {pallets[0]}");
+Console.WriteLine($"Before: {pallets[0].ToLower()}");
+//Array.Clear(pallets, 0, 2);
+//Console.WriteLine($"After: {pallets[0].ToLower()}"); // will cause null ref if elements are empty
 
 Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
 foreach (var pallet in pallets)
 {
-    Console.WriteLine($"-- {pallet}");
+    Console.WriteLine($"-- {pallet}"); 
 }
 
 /* END */
