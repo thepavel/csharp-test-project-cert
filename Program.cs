c7d912b62c56998f9901365f97571cc1fb372fd8
pavel <lutskovp@gmail.com>
2023-10-26 00:06:37 -0700

    start exercise in string interpolation

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿
+﻿int invoiceNumber = 1201;
+decimal productShares = 25.4568m;
+decimal subtotal = 2750.00m;
+decimal taxPercentage = .15825m;
+decimal total = 3185.19m;
+
+Console.WriteLine($"Invoice Number: {invoiceNumber}");
 
 #region StringFormat
     
