4e3d128b9f32bd1cd22caba80c0881fdd6963650
pavel <lutskovp@gmail.com>
2023-10-26 00:26:49 -0700

    finish padding

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
 ﻿
 string paymentId = "769C";
- string payeeName = "Mr. Stephen Ortega";
+string payeeName = "Mr. Stephen Ortega";
+string paymentAmount = "$5,000.00";
 
- var formattedLine = paymentId.PadRight(6);
- formattedLine += payeeName.PadRight(24);
+var formattedLine = paymentId.PadRight(6);
+formattedLine += payeeName.PadRight(24);
+formattedLine += paymentAmount.PadLeft(10);
 
- Console.WriteLine(formattedLine);
+Console.WriteLine("1234567890123456789012345678901234567890");
+Console.WriteLine(formattedLine);
 
 #region StringFormat
     
