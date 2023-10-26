34a552f7d98e59b411085487fe954ba0980c6ade
pavel <lutskovp@gmail.com>
2023-10-26 00:12:23 -0700

    recycle old examples

--- Program.cs
+++ Program.cs
@@ -1,14 +1,4 @@
-﻿int invoiceNumber = 1201;
-decimal productShares = 25.4568m;
-decimal subtotal = 2750.00m;
-decimal taxPercentage = .15825m;
-decimal total = 3185.19m;
-
-Console.WriteLine($"Invoice Number: {invoiceNumber}");
-Console.WriteLine($"   Shares: {productShares:N3} Product");
-Console.WriteLine($"     Sub Total: {subtotal:C}");
-Console.WriteLine($"           Tax: {taxPercentage:P2}");
-Console.WriteLine($"     Total Billed: {total:C}");
+﻿
 
 #region StringFormat
     
@@ -39,5 +29,17 @@ Console.WriteLine($"     Total Billed: {total:C}");
 
         // message += $"A discount of {discount / price:P2}!"; //inserted
         // Console.WriteLine(message);
+
+        // int invoiceNumber = 1201;
+        // decimal productShares = 25.4568m;
+        // decimal subtotal = 2750.00m;
+        // decimal taxPercentage = .15825m;
+        // decimal total = 3185.19m;
+
+        // Console.WriteLine($"Invoice Number: {invoiceNumber}");
+        // Console.WriteLine($"   Shares: {productShares:N3} Product");
+        // Console.WriteLine($"     Sub Total: {subtotal:C}");
+        // Console.WriteLine($"           Tax: {taxPercentage:P2}");
+        // Console.WriteLine($"     Total Billed: {total:C}");
     
 #endregion
