61e9b1100335b27d4cadbe8a57807a07f0e72aa4
pavel <lutskovp@gmail.com>
2023-10-26 00:05:23 -0700

    move completed exercises

--- Program.cs
+++ Program.cs
@@ -1,20 +1,5 @@
 ﻿
 
-decimal tax = .36785m;
-Console.WriteLine($"Tax rate: {tax:P2}");
-// Tax rate: 36.79 %
-
-// combination
-
-decimal price = 67.55m;
-decimal salePrice = 59.99m;
-decimal discount = price - salePrice;
-
-string message = String.Format("You saved {0:C2} off the regular {1:C2} price. ", discount, price);
-
-message += $"A discount of {discount / price:P2}!"; //inserted
-Console.WriteLine(message);
-
 #region StringFormat
     
         // string first = "Hello";
@@ -28,5 +13,21 @@ Console.WriteLine(message);
     
         // decimal measurement = 123456.78912m;
         // Console.WriteLine($"Measurement: {measurement:N4} units"); 
+
+
+        // decimal tax = .36785m;
+        // Console.WriteLine($"Tax rate: {tax:P2}");
+        // // Tax rate: 36.79 %
+
+        // // combination
+
+        // decimal price = 67.55m;
+        // decimal salePrice = 59.99m;
+        // decimal discount = price - salePrice;
+
+        // string message = String.Format("You saved {0:C2} off the regular {1:C2} price. ", discount, price);
+
+        // message += $"A discount of {discount / price:P2}!"; //inserted
+        // Console.WriteLine(message);
     
 #endregion
