df6af71bd8a0cd791eb413cac7be5443559bf41f
pavel <lutskovp@gmail.com>
2023-10-26 00:22:45 -0700

    create formatted line

--- Program.cs
+++ Program.cs
@@ -1,4 +1,11 @@
 ﻿
+string paymentId = "769C";
+ string payeeName = "Mr. Stephen Ortega";
+
+ var formattedLine = paymentId.PadRight(6);
+ formattedLine += payeeName.PadRight(24);
+
+ Console.WriteLine(formattedLine);
 
 #region StringFormat
     
