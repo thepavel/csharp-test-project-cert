6655c60275680b9d1d905f5b3aed11ae8ad04dd8
pavel <lutskovp@gmail.com>
2023-10-25 23:38:55 -0700

    number formatting rounds up correctly for decimal

--- Program.cs
+++ Program.cs
@@ -7,3 +7,6 @@ Console.WriteLine(result);
 decimal price = 123.45m;
 int discount = 50;
 Console.WriteLine($"Price: {price:C} (Save {discount:C})");
+
+decimal measurement = 123456.78912m;
+Console.WriteLine($"Measurement: {measurement:N2} units");
