4a1fd3521c4013683ab11a9c2c5dc57155cce3c0
pavel <lutskovp@gmail.com>
2023-10-25 23:45:28 -0700

    combine approaches, math in string.format

--- Program.cs
+++ Program.cs
@@ -4,9 +4,9 @@ string second = "World";
 string result = string.Format("{0} {1}!", first, second);
 Console.WriteLine(result);
 
-decimal price = 123.45m;
-int discount = 50;
-Console.WriteLine($"Price: {price:C} (Save {discount:C})");
+// decimal price = 123.45m;
+// int discount = 50;
+// Console.WriteLine($"Price: {price:C} (Save {discount:C})");
 
 decimal measurement = 123456.78912m;
 Console.WriteLine($"Measurement: {measurement:N4} units");
@@ -14,3 +14,12 @@ Console.WriteLine($"Measurement: {measurement:N4} units");
 decimal tax = .36785m;
 Console.WriteLine($"Tax rate: {tax:P2}");
 // Tax rate: 36.79 %
+
+// combination
+
+decimal price = 67.55m;
+decimal salePrice = 59.99m;
+
+string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} price. ", (price - salePrice), price);
+
+Console.WriteLine(yourDiscount);
