9690340f6d1e90692235fc220d733b7bd321aa72
pavel <lutskovp@gmail.com>
2023-10-25 23:33:41 -0700

    string.format

--- Program.cs
+++ Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("console app blank");
+﻿
+string first = "Hello";
+string second = "World";
+string result = string.Format("{0} {1}!", first, second);
+Console.WriteLine(result);
+
