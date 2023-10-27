fad392d827f9eac7ad17142fe9438b4ebaaee05c
pavel <lutskovp@gmail.com>
2023-10-26 22:38:22 -0700

    wip: begin formatting message with string interpolation

--- Program.cs
+++ Program.cs
@@ -15,6 +15,50 @@ Glorious Future     13.13 %   $63,000,000.00
 
 */
 
+const string expected = @"Dear Ms. Barros,
+As a customer of our Magic Yield offering we are excited to tell you about a new financial product that would dramatically increase your return.
+
+Currently, you own 2,975,000.00 shares at a return of 12.75 %.
+
+Our new product, Glorious Future offers a return of 13.13 %.  Given your current volume, your potential profit would be ¤63,000,000.00.
+
+Here's a quick comparison:
+
+Magic Yield         12.75 %   $55,000,000.00      
+Glorious Future     13.13 %   $63,000,000.00";
+
+string customerName = "Ms. Barros";
+
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+var message = @$"Dear {customerName},
+As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.
+
+Currently, you own {currentShares:N2} shares at a return of {currentReturn:P2}.
+
+Our new product, Glorious Future offers a return of 13.13 %.  Given your current volume, your potential profit would be ¤63,000,000.00.";
+
+Console.WriteLine(message);
+Console.WriteLine(string.Compare(expected, message));
+//Here's a quick comparison:
+
+Console.WriteLine("Here's a quick comparison:\n");
+
+string comparisonMessage = "";
+
+// Magic Yield         12.75 %   $55,000,000.00      
+// Glorious Future     13.13 %   $63,000,000.00";
+
+// Your logic here
+
+Console.WriteLine(comparisonMessage);
 
 #region StringFormat
     
