a4c6a147efbeee528bc985fa5ffa36bf3afdc1a2
pavel <lutskovp@gmail.com>
2023-10-28 23:46:12 -0700

    lastIndexOf

--- Program.cs
+++ Program.cs
@@ -1,14 +1,27 @@
-﻿string message = "What is the value <span>between the tags</span>?";
+﻿
+string message = "(What if) I am (only interested) in the last (set of parentheses)";
 
-const string openingSymbol = "<span>";
-const string closingSymbol = "</span>";
+const string openingSymbol = "(";
+const string closingSymbol = ")";
 
-int openingPosition = message.IndexOf(openingSymbol);
-int closingPosition = message.IndexOf(closingSymbol);
+int lastOpeningPosition = message.LastIndexOf(openingSymbol);
+int lastclosingPosition = message.LastIndexOf(closingSymbol);
 
-openingPosition += openingSymbol.Length;
+lastOpeningPosition += 1;
 
-Console.WriteLine(openingPosition);
-Console.WriteLine(closingPosition);
+Console.WriteLine(message[lastOpeningPosition..lastclosingPosition]);
 
-Console.WriteLine(message[openingPosition..closingPosition]);
+// string message = "What is the value <span>between the tags</span>?";
+
+// const string openingSymbol = "<span>";
+// const string closingSymbol = "</span>";
+
+// int openingPosition = message.IndexOf(openingSymbol);
+// int closingPosition = message.IndexOf(closingSymbol);
+
+// openingPosition += openingSymbol.Length;
+
+// Console.WriteLine(openingPosition);
+// Console.WriteLine(closingPosition);
+
+// Console.WriteLine(message[openingPosition..closingPosition]);
