3c20e725e61c6068e9cf907c2ae4f0faf2ecd2c9
pavel <lutskovp@gmail.com>
2023-10-28 22:41:06 -0700

    work with string.indexOf

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("console app blank");
+﻿string message = "Find what is (inside the parentheses)";
+
+int openingPosition = message.IndexOf('(');
+int closingPosition = message.IndexOf(')');
+
+openingPosition += 1;
+
+Console.WriteLine(openingPosition);
+Console.WriteLine(closingPosition);
+
+Console.WriteLine(message.Substring(openingPosition, closingPosition - openingPosition));
