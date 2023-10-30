ab830f25bd129f626cc93ef53c12c03af55fdb54
pavel <lutskovp@gmail.com>
2023-10-30 00:02:28 -0700

    wip: begin replace challenge

--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
-﻿string data = "12345John Smith          5000  3  ";
+﻿
+const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
 
-string updatedData = data.Remove(5, 20);
-Console.WriteLine(updatedData);
+string quantity = "";
+string output = "";
+
+// Your work here
+
+Console.WriteLine(quantity);
+Console.WriteLine(output);
+
+// string data = "12345John Smith          5000  3  ";
+
+// string updatedData = data.Remove(5, 20);
+// Console.WriteLine(updatedData);
 
 
 //string message = "(What if) I am (only interested) in the last (set of parentheses)";
