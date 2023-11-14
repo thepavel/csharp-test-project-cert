ce77ec9cad95aa9f40f2e76c23f2b427dbb49b12
pavel <lutskovp@gmail.com>
2023-11-13 21:50:44 -0800

    create basic method

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
 ﻿// https://learn.microsoft.com/en-us/training/modules/write-first-c-sharp-method/3-exercise-create-your-first-method
 
-Console.WriteLine("console app blank");
+Console.WriteLine("Generating random numbers:");
+DisplayRandomNumbers();
 
-void DisplayRandomNumbers() {
+static void DisplayRandomNumbers() {
+    Random random = new Random();
 
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine($"{random.Next(1,100)} ");
+    }
+    Console.WriteLine();
 }
